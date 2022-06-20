43f9b926e5881acdc4be60e17f74b97464c3c3f2
Nick <Nick_II2000@mail.ru>
2022-06-20 08:37:45 +0600

    3-th commit

--- js/script.js
+++ js/script.js
@@ -160,12 +160,13 @@ window.addEventListener('DOMContentLoaded', () => {
     // Используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 60;
             this.changeToRUB();
@@ -177,17 +178,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            this.classes.forEach(className => {
+                element.classList.add(className);
+            });
             element.innerHTML = `
-                <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price} </span>руб./день</div>
-                    </div>
-                </div>`;
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price} </span>руб./день</div>
+                </div>
+            `;
             this.parent.append(element);
         }
 
